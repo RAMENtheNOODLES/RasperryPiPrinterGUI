046971e54e15cea0bb312c4f4463a667f8d738ab
cookiejar <rommecar000@bdusd.org>
2024-03-28 16:57:26 -0500

    Did stuff

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_subdirs():
 
     subdirs = ""
     for directory in sub_dir:
-        #print(f"Processing {directory}")
+        print(f"Processing {directory}")
         subdirs += directory + "/"
 
     return subdirs
